ac50f7b378ccb3bcd2e063e9123862a27b8be593
wonglok <yellowhappy831@gmail.com>
2020-03-31 08:47:54 +0800

    save

--- src/http/get-env/get-web-socket-url.js
+++ src/http/get-env/get-web-socket-url.js
@@ -5,8 +5,8 @@
 module.exports = function getWS() {
   let env = process.env.NODE_ENV
   let testing = 'ws://localhost:3333'
-  let staging = 'wss://api-staging.wonglok.com/'
-  let production = 'wss://api.wonglok.com/'
+  let staging = 'ws://api-staging.wonglok.com/'
+  let production = 'ws://api.wonglok.com/'
   if (env === 'testing')
     return testing
   if (env === 'staging')
